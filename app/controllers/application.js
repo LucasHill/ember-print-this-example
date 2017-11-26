76dcd82cb4f9d556a48c0c7ab799ad0deb2f3114
Lucas Hill <kingofallhills@gmail.com>
2017-11-26 01:12:47 -0800

    Add custom selector example.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,10 @@
 import Controller from '@ember/controller';
 import { inject as service } from "@ember/service";
+import { computed } from '@ember/object';
+
 export default Controller.extend({
   router: service(),
+  currentRouteName: computed('router.currentRouteName', function() {
+    return this.get('router.currentRouteName');
+  })
 });
--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('basic-auto');
+  this.route('custom-selector');
 });
 
 export default Router;
